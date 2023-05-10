c74bb58483ce2ab66bad3f86572036ab3a90060f
Shani <shanigeva12@gmail.com>
2023-05-11 00:33:31 +0300

    added control comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ScreenComponent } from './components/screen/screen.component';
+import { ControlComponent } from './components/control/control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenComponent,
+    ControlComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
