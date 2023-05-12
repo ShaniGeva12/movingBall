4acdd750c5ae63885d6ec6028d5c3c773625b1f5
Shani <shanigeva12@gmail.com>
2023-05-13 00:45:06 +0300

    paint changes to screen

--- src/app/components/screen/screen.component.ts
+++ src/app/components/screen/screen.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Control } from '../control/model/control';
+import { ControlStatusService } from '../control/services/control-status.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-screen',
@@ -6,6 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./screen.component.scss'],
 })
 export class ScreenComponent {
+  controlStatus$: Observable<Control> = this.controlStatusService.status$;
+  statSub: Subscription | undefined;
   left = 0;
   top = 0;
+
+  constructor(private controlStatusService: ControlStatusService) {}
+
+  ngOnInit(): void {
+    this.statSub = this.controlStatus$.subscribe((newStat) => {
+      // multiply by 10 to better see changes on screen
+      this.left = -newStat.left * 10;
+      this.top = -newStat.up * 10;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.statSub?.unsubscribe();
+  }
 }
