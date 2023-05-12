1b9b4ba4d6294999a20d1fa4ad07966349221c68
Shani <shanigeva12@gmail.com>
2023-05-13 00:44:49 +0300

    control comp works

--- src/app/components/control/control.component.ts
+++ src/app/components/control/control.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { ControlStatusService } from './services/control-status.service';
+import { ControllerButton } from './model/controller-button';
+import { Control } from './model/control';
 
 @Component({
   selector: 'app-control',
   templateUrl: './control.component.html',
-  styleUrls: ['./control.component.scss']
+  styleUrls: ['./control.component.scss'],
 })
 export class ControlComponent {
+  controllerButton = ControllerButton;
+  control: Control = {
+    up: 0,
+    left: 0,
+  };
 
+  constructor(private controlStatusService: ControlStatusService) {}
+
+  onButtonClicked(btn: ControllerButton): void {
+    switch (btn) {
+      case 'up':
+        this.control.up++;
+        break;
+      case 'down':
+        this.control.up--;
+        break;
+      case 'left':
+        this.control.left++;
+        break;
+      case 'right':
+        this.control.left--;
+        break;
+      case 'reset':
+        this.control.left = 0;
+        this.control.up = 0;
+        break;
+      default:
+        break;
+    }
+
+    this.controlStatusService.updateControlStatus(this.control);
+  }
 }
