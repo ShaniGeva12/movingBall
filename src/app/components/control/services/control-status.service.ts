013416caae7c988691fcd596ae76210e295378f2
Shani <shanigeva12@gmail.com>
2023-05-13 00:44:35 +0300

    service updated

--- src/app/components/control/services/control-status.service.ts
+++ src/app/components/control/services/control-status.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Control } from '../model/control';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ControlStatusService {
+  private _status$: BehaviorSubject<Control> = new BehaviorSubject<Control>({
+    up: 0,
+    left: 0,
+  });
+  public status$: Observable<Control> = this._status$.asObservable();
 
-  constructor() { }
+  constructor() {}
+
+  updateControlStatus(newStatus: Control): void {
+    this._status$.next(newStatus);
+  }
 }
